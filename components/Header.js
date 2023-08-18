6112e40ad48e8307545348341cc877bce90427ff
Rio Boy <boyrio68@gmail.com>
2023-08-19 01:52:55 +0800

    setting color-pallete, export assets from figma, and create component => Header

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="container">
+        Header Components
+    </div>
+  )
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,40 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          xl: '9.375rem',
+        },
+      },
+      colors: {
+        primary: '#383838',
+        secondary: '#B2B2B2',
+        'lemonade': {
+          50: '#f6f9d6',
+          100: '#fbfec5',
+          200: '#fbff9a',
+          300: '#fffd73',
+          400: '#ffed52',
+          500: '#f6c937',
+          600: '#d88d23',
+          700: '#af4e17',
+          800: '#7d1b10',
+          900: '#450914',
+        },
+        'aragon': {
+          50: '#d4f1d6',
+          100: '#c1f0cb',
+          200: '#9eebbf',
+          300: '#7be4b7',
+          400: '#5cd7a7',
+          500: '#42c486',
+          600: '#2da952',
+          700: '#23871e',
+          800: '#396012',
+          900: '#363409',
+        }
       },
     },
   },
